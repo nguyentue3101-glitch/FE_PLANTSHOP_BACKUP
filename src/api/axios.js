232c4d3348333bc7c8c918deaf994d747f3407be
TueNguyen <keynguyen3101@gmail.com>
2026-01-12 20:19:21 +0700

    create  API_BASE_URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
